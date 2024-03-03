764139f3d78fc38e7f205219522e22c56a285697
Dimitar Mihaylov <dimitar.mihaylove@gmail.com>
2024-03-03 22:19:39 +0200

    New layout with left navigation

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -7,16 +7,47 @@ import {NgClass} from "@angular/common";
 import {ThemeService} from "./core/theme/theme.service";
 import {ThemePickerComponent} from "./core/theme/theme-picker/theme-picker.component";
 import {NavbarComponent} from "./core/components/navbar/navbar.component";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, MatButton, MatSidenavContainer, MatSidenav, MatSidenavContent, MatToolbar, RouterLink, MatAnchor, NgClass, RouterLinkActive, ThemePickerComponent, NavbarComponent],
+    imports: [RouterOutlet,
+        MatButton, MatSidenavContainer, MatSidenav, MatSidenavContent, MatToolbar, RouterLink, MatAnchor, NgClass, RouterLinkActive, ThemePickerComponent, NavbarComponent, MatIconModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
     open = false
     title = 'homeStock';
+
+    links = [
+        {
+            label: "Dashboard",
+            icon: "dashboard",
+            route: "/dashboard"
+        },
+        {
+            label: "Expenses",
+            icon: "payments",
+            route: "/expenses"
+        },
+        {
+            label: "Savings",
+            icon: "savings",
+            route: "/savings"
+        },
+        {
+            label: "Items",
+            icon: "inventory_2",
+            route: "/items"
+        },
+        {
+            label: "Tags",
+            icon: "sell",
+            route: "/tags"
+        },
+    ]
+
     themeService = inject(ThemeService);
 }
